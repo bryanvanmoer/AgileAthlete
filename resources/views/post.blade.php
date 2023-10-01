73e1aeffaecc5af124d1be38beefc278eb04cbb2
bryanvanmoer <bryan.vanmoer51@hotmail.com>
2023-10-01 21:14:28 +0200

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Mariti utilia deberet haec lenitate exitium erga regina in obstinatum rettulimus actibus cum et actibus
+            Gordianorum in similia multa potius feminea utilia potius reducere coniugem exitium admovente in Gordianorum
+            rettulimus. Mariti utilia deberet haec lenitate exitium erga regina in obstinatum rettulimus actibus cum et actibus
+            Gordianorum in similia multa potius feminea utilia potius reducere coniugem exitium admovente in Gordianorum
+            rettulimus.
+        </p>
+    </article>
+    <a href="/">Go Back</a>
+
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Mariti utilia deberet haec lenitate exitium erga regina in obstinatum rettulimus actibus cum et actibus
+            Gordianorum in similia multa potius feminea utilia potius reducere coniugem exitium admovente in Gordianorum
+            rettulimus. Mariti utilia deberet haec lenitate exitium erga regina in obstinatum rettulimus actibus cum et actibus
+            Gordianorum in similia multa potius feminea utilia potius reducere coniugem exitium admovente in Gordianorum
+            rettulimus.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>
+            Mariti utilia deberet haec lenitate exitium erga regina in obstinatum rettulimus actibus cum et actibus
+            Gordianorum in similia multa potius feminea utilia potius reducere coniugem exitium admovente in Gordianorum
+            rettulimus. Mariti utilia deberet haec lenitate exitium erga regina in obstinatum rettulimus actibus cum et actibus
+            Gordianorum in similia multa potius feminea utilia potius reducere coniugem exitium admovente in Gordianorum
+            rettulimus.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>
+            Mariti utilia deberet haec lenitate exitium erga regina in obstinatum rettulimus actibus cum et actibus
+            Gordianorum in similia multa potius feminea utilia potius reducere coniugem exitium admovente in Gordianorum
+            rettulimus. Mariti utilia deberet haec lenitate exitium erga regina in obstinatum rettulimus actibus cum et actibus
+            Gordianorum in similia multa potius feminea utilia potius reducere coniugem exitium admovente in Gordianorum
+            rettulimus.
+        </p>
+    </article>
+
+</body>
