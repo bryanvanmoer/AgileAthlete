e0fa98a069215f166e2135cd8c0df721eac3e1ae
bryanvanmoer <bryan.vanmoer51@hotmail.com>
2023-10-11 23:00:14 +0200

    design and integration

--- resources/views/addresses.blade.php
+++ resources/views/addresses.blade.php
@@ -1,5 +1,23 @@
 <x-layout>
-    @foreach($addresses as $address)
+    @include('_address-header');
+
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-address-featured-card :address="$addresses[0]"/>
+
+{{--        <div class="lg:grid lg:grid-cols-2">
+            <x-address-card/>
+            <x-address-card/>
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-address-card/>
+            <x-address-card/>
+            <x-address-card/>
+        </div>--}}
+    </main>
+
+    {{--    @foreach($addresses as $address)
         <article>
             <h1>
                 <a href="/addresses/{{ $address->id }}">
@@ -11,5 +29,5 @@
                 {{ $address->city }}, {{$address->postal_code}}
             </div>
         </article>
-    @endforeach
+    @endforeach--}}
 </x-layout>
